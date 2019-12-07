12a0bbd6b07ce34a2dc444bafe287d8300ae582c
wuwj-cn <wenjie.0617@gmail.com>
2019-12-07 23:17:04 +0800

    通用模块添加copyright

--- mplus-common/src/main/java/com/mplus/common/response/ResponseStatus.java
+++ mplus-common/src/main/java/com/mplus/common/response/ResponseStatus.java
@@ -1,7 +1,29 @@
+/*
+ * Copyright 2018-2019 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+
 package com.mplus.common.response;
 
+/**
+ * 通用模块响应状态定义，以90xx定义
+ *
+ * @author wuwj [254513235@qq.com]
+ * @since 1.0
+ */
 public enum ResponseStatus {
-	SUCCESS(900, "success"), AUTH_NOT_PASSED(901, "authentication is not passed");
+	SUCCESS(9000, "success"), AUTH_NOT_PASSED(9001, "authentication is not passed");
 
 	private final int value;
 	private final String reasonPhrase;
