5efff651befc9c10efd08b848b06697f6b1f95aa
wuwj-cn <wenjie.0617@gmail.com>
2019-12-07 20:49:30 +0800

    1. 添加RequestIdFilter，在header中添加request_id值 2. 修复fastJson消息转换器无法生效的bug

--- mplus-common/src/main/java/com/mplus/common/config/WebMvcConfig.java
+++ mplus-common/src/main/java/com/mplus/common/config/WebMvcConfig.java
@@ -1,48 +1,97 @@
+/*
+ * Copyright 2018-2019 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+
 package com.mplus.common.config;
 
 import com.alibaba.fastjson.serializer.SerializerFeature;
-import com.alibaba.fastjson.serializer.ValueFilter;
 import com.alibaba.fastjson.support.config.FastJsonConfig;
 import com.alibaba.fastjson.support.spring.FastJsonHttpMessageConverter;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
+import org.springframework.boot.autoconfigure.http.HttpMessageConverters;
+import org.springframework.boot.web.servlet.ServletComponentScan;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.MediaType;
 import org.springframework.http.converter.HttpMessageConverter;
+import org.springframework.http.converter.StringHttpMessageConverter;
+import org.springframework.web.servlet.DispatcherServlet;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+import javax.servlet.Servlet;
+import java.nio.charset.StandardCharsets;
 import java.util.ArrayList;
 import java.util.List;
 
+@Slf4j
 @Configuration
-public class WebMvcConfig extends WebMvcConfigurationSupport {
-
-    @Override
-    public void configureMessageConverters(List<HttpMessageConverter<?>> converters) {
-        super.configureMessageConverters(converters);
-
-        FastJsonHttpMessageConverter fastConverter = new FastJsonHttpMessageConverter();
-        FastJsonConfig fastJsonConfig = new FastJsonConfig();
-        fastJsonConfig.setSerializerFeatures(SerializerFeature.PrettyFormat);
-        // 过滤器处理
-        ValueFilter filter = (clazz, name, value) -> {
-			if (clazz.getClass().isEnum()) {
-				return ((Enum<?>) clazz).ordinal();
-			}
-			return name;
-		};
-        fastJsonConfig.setSerializeFilters(filter);
-        fastConverter.setFastJsonConfig(fastJsonConfig);
-
-        // 处理中文乱码问题
-        List<MediaType> fastMediaTypes = new ArrayList<>();
-        fastMediaTypes.add(MediaType.APPLICATION_JSON_UTF8);
-        fastConverter.setSupportedMediaTypes(fastMediaTypes);
-        converters.add(fastConverter);
-    }
+@ConditionalOnClass({Servlet.class, DispatcherServlet.class, WebMvcConfigurer.class})
+@ServletComponentScan(basePackages = "com.mplus.common.filter")
+public class WebMvcConfig implements WebMvcConfigurer {
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**");
     }
 
+    @Override
+    public void extendMessageConverters(List<HttpMessageConverter<?>> converters) {
+        for (HttpMessageConverter<?> messageConverter : converters) {
+            log.info(messageConverter.toString());
+        }
+    }
+
+    /**
+     * 创建fastJson的消息转换器
+     * 如果直接重写extendMessageConverters时，会因fastJson转换器被添加在MappingJackson2HttpMessageConverter后面，
+     * 导致fastJson消息转换器无法生效
+     *
+     * @return
+     */
+    @Bean
+    public HttpMessageConverters fastJsonHttpMessageConverters() {
+        /**
+         * 1.先定义一个convert转换消息的对象
+         * 2.添加fastjson的配置信息，比如：是否要格式化返回的json数据
+         * 3.在convert中添加配置信息
+         * 4.将convert添加到converters当中
+         */
+        // 1.先定义一个convert转换消息的对象
+        FastJsonHttpMessageConverter fastConverter = new FastJsonHttpMessageConverter();
+        // 2.添加fastjson的配置信息，比如：是否要格式化返回的json数据
+        FastJsonConfig fastJsonConfig = new FastJsonConfig();
+        fastJsonConfig.setSerializerFeatures(
+                SerializerFeature.PrettyFormat,
+                SerializerFeature.WriteNullListAsEmpty,
+                SerializerFeature.WriteMapNullValue,
+                SerializerFeature.WriteNullStringAsEmpty
+        );
+
+        // 处理中文乱码问题(不然出现中文乱码)
+        List<MediaType> fastMediaTypes = new ArrayList<MediaType>();
+        fastMediaTypes.add(MediaType.APPLICATION_JSON_UTF8);
+        fastConverter.setSupportedMediaTypes(fastMediaTypes);
+
+        // 3.在convert中添加配置信息
+        fastConverter.setFastJsonConfig(fastJsonConfig);
+
+        // 修改StringHttpMessageConverter默认配置
+        StringHttpMessageConverter converter = new StringHttpMessageConverter(StandardCharsets.UTF_8);
+
+        return new HttpMessageConverters(fastConverter);
+    }
 }
--- mplus-common/src/main/java/com/mplus/common/filter/RequestIdFilter.java
+++ mplus-common/src/main/java/com/mplus/common/filter/RequestIdFilter.java
@@ -8,88 +8,41 @@
  *      http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
  */
 
 package com.mplus.common.filter;
 
-import javax.servlet.*;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
 import javax.servlet.annotation.WebFilter;
 import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletRequestWrapper;
+import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
-import java.util.*;
+import java.util.UUID;
 
 /**
  * RequestIdFilter
  *
- * @author wuwj
+ * @author wuwj-cn [wenjie0617@163.com]
  * @since 1.0
  */
-@WebFilter(urlPatterns = { "/*" }, filterName = "requestIdFilter")
-public class RequestIdFilter implements Filter {
+@Slf4j
+@WebFilter(urlPatterns = "/*", filterName = "requestIdFilter")
+public class RequestIdFilter extends OncePerRequestFilter {
     @Override
-    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
-        HeaderMapRequestWrapper requestWrapper = new HeaderMapRequestWrapper((HttpServletRequest) request);
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+        HeaderMapRequestWrapper requestWrapper = new HeaderMapRequestWrapper(request);
 
         String requestId = UUID.randomUUID().toString().replace("-", "");
-        requestWrapper.addHeader("requestId",requestId);
-        chain.doFilter(requestWrapper, response);
-    }
-}
-
-class HeaderMapRequestWrapper extends HttpServletRequestWrapper {
-    /**
-     * construct a wrapper for this request
-     *
-     * @param request
-     */
-    public HeaderMapRequestWrapper(HttpServletRequest request) {
-        super(request);
-    }
-
-    private Map<String, String> headerMap = new HashMap<>();
-
-    /**
-     * add a header with given name and value
-     *
-     * @param name
-     * @param value
-     */
-    public void addHeader(String name, String value) {
-        headerMap.put(name, value);
-    }
-
-    @Override
-    public String getHeader(String name) {
-        String headerValue = super.getHeader(name);
-        if (headerMap.containsKey(name)) {
-            headerValue = headerMap.get(name);
-        }
-        return headerValue;
-    }
-
-    /**
-     * get the Header names
-     */
-    @Override
-    public Enumeration<String> getHeaderNames() {
-        List<String> names = Collections.list(super.getHeaderNames());
-        for (String name : headerMap.keySet()) {
-            names.add(name);
-        }
-        return Collections.enumeration(names);
-    }
-
-    @Override
-    public Enumeration<String> getHeaders(String name) {
-        List<String> values = Collections.list(super.getHeaders(name));
-        if (headerMap.containsKey(name)) {
-            values = Arrays.asList(headerMap.get(name));
-        }
-        return Collections.enumeration(values);
+        requestWrapper.addHeader("requestId", requestId);
+        log.info("custom request_id filter load complete");
+        filterChain.doFilter(requestWrapper, response);
     }
 }
