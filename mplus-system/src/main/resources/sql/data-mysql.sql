483c348d51ed7db9ae0f8c60a218dde79d1042d0
wuwj-cn <wenjie.0617@gmail.com>
2019-12-13 18:27:59 +0800

    增加启动自动加载sql配置

---
+++ mplus-system/src/main/resources/sql/data-mysql.sql
@@ -0,0 +1,6 @@
+/**
+ * 数据初始化脚本
+ * @date 2019/08/12
+ * @auth wuwj
+ */
+INSERT INTO mp_sys_org (id, creator_id, create_time, data_state, operator_id, modify_time, org_id, org_name, parent_org_id, remark) VALUES ('0', null, null, '0', null, null, '0', 'ROOT', null, 'ROOT');
